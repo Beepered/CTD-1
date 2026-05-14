4078330d1e2da8f460e0c340f7f18ca49b674707
Beepered <bktrieu@gmail.com>
2026-05-14 14:44:55 -0700

    logon page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import "./App.css";
 import { useState } from "react";
 import Header from "./shared/Header";
 import TodoPage from "./features/Todos/TodoPage";
+import Logon from "./features/Logon";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+
   return (
     <div>
       <Header></Header>
-      <TodoPage></TodoPage>
+      {token ? (
+        <TodoPage token={token}></TodoPage>
+      ) : (
+        <Logon onSetEmail={setEmail} onSetToken={setToken}></Logon>
+      )}
     </div>
   );
 }
